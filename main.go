3d0166e5a2d812dcb7441dcb184cab5e6080f54a
Lexert19 <dominikch19@gmail.com>
2026-08-04 23:03:52 +0200

    added config file

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"time"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
+type Config struct {
+	TargetURL   string        `yaml:"target_url" env:"TARGET_URL"`
+	Timeout     time.Duration `yaml:"timeout"`
+	Concurrency int           `yaml:"concurrency"`
+}
+
+
 func main() {
+	var cfg Config
+	if err := cleanenv.ReadConfig("config.yaml", &cfg); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 	fetcher := NewMultiFetcher(
 		NewURLTextFetcher("https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt", SOCKS5),
 		NewURLTextFetcher("https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt", HTTP),
@@ -16,8 +29,9 @@ func main() {
 	)
 
 	repo := NewFileRepository()
-	checker := NewChecker("http://dominik-chyziak.pl", 5*time.Second)
-	engine := NewEngine(checker, repo, 200)
+	checker := NewChecker(cfg.TargetURL, cfg.Timeout)
+	engine := NewEngine(checker, repo, cfg.Concurrency)
+
 
 	ctx := context.Background()
 	proxies, err := fetcher.Fetch(ctx)
